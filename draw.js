694ddb06f6c7162a4066a5152b2d879c1de7356a
YangXuanqi <1354570828@qq.com>
2024-10-12 11:13:11 +0800

    绘制停留点 绘制离线路段

--- draw.js
+++ draw.js
@@ -1,48 +1,75 @@
 class DrawMap {
-    constructor(container, centalPoint) {
-        // 创建地图实例 
-        this.map = new BMapGL.Map(container, {
-            enableRotate: false,
-            enableTilt: false
-        });
-        var point = new BMapGL.Point(centalPoint.longitude, centalPoint.latitude);
-        this.map.centerAndZoom(point, 19);
-        this.map.enableScrollWheelZoom(true);
-    }
-    drawLine (points) {
-        var node = new BMapGL.Icon('//mapopen-pub-jsapigl.bj.bcebos.com/demoimg/zhongheyiyuan.png', new BMapGL.Size(11, 11), {
-            offset: new BMapGL.Size(5, 5)
-        });
-        //创建折线
-        var polyline = new BMapGL.Polyline(points.map(p => new BMapGL.Point(p.longitude, p.latitude)), {
-            strokeStyle: 'dashed',
-            strokeColor: 'blue',
-            strokeTexture: {
-                url: 'https://mapopen-pub-jsapigl.bj.bcebos.com/svgmodel/Icon_road_blue_arrow.png',
-                width: 16,
-                height: 64
-            },
-            strokeWeight: 8,
-            strokeOpacity: 0.8,
-            node: node
-        });
-        this.map.addOverlay(polyline);
-    }
-    drawMarker (p) {
-        var marker = new BMapGL.Marker(new BMapGL.Point(p.longitude, p.latitude), {
-            title: p.index
-        });
-        // 创建文本标注
-        var label = new BMapGL.Label(p.ds, {
-            position: new BMapGL.Point(p.longitude, p.latitude),
-            offset: new BMapGL.Size(-20, -20)
-        })
-        label.setStyle({                              // 设置label的样式
-            color: '#000',
-            fontSize: '10px',
-            border: '1px solid #1E90FF'
-        })
-        this.map.addOverlay(label);
-        this.map.addOverlay(marker);
-    }
+  constructor(container, centalPoint) {
+    // 创建地图实例
+    this.map = new BMapGL.Map(container, {
+      enableRotate: false,
+      enableTilt: false,
+    });
+    var point = new BMapGL.Point(centalPoint.longitude, centalPoint.latitude);
+    this.map.centerAndZoom(point, 19);
+    this.map.enableScrollWheelZoom(true);
+  }
+
+  drawLine(points, color) {
+    color = color ? color : "blue";
+    var node = new BMapGL.Icon(
+      "//mapopen-pub-jsapigl.bj.bcebos.com/demoimg/zhongheyiyuan.png",
+      new BMapGL.Size(11, 11),
+      {
+        offset: new BMapGL.Size(5, 5),
+      }
+    );
+
+    //创建折线
+    var polyline = new BMapGL.Polyline(
+      points.map((p) => new BMapGL.Point(p.longitude, p.latitude)),
+      {
+        strokeStyle: "dashed",
+        strokeTexture: {
+          url: `https://mapopen-pub-jsapigl.bj.bcebos.com/svgmodel/Icon_road_${color}_arrow.png`,
+          width: 16,
+          height: 64,
+        },
+        strokeWeight: 8,
+        strokeOpacity: 0.8,
+        node: node,
+      }
+    );
+    this.map.addOverlay(polyline);
+  }
+  drawPath(points) {
+    this.drawLine(points);
+    points.forEach((p, i) => {
+      if (p.reconnect) {
+        this.drawLine([points[i - 1], p], "red");
+        this.drawMarker(
+          p,
+          `离线时间:${
+            (new Date(p.time) - new Date(points[i - 1].time)) /
+            (60 * 1000).toFixed(1)
+          }分钟;离线距离:${Math.round(p.ds)}米`,
+          'red'
+        );
+      }
+    });
+  }
+
+  drawMarker(p, text, color) {
+    var marker = new BMapGL.Marker(new BMapGL.Point(p.longitude, p.latitude), {
+      title: p.time,
+    });
+    // 创建文本标注
+    var label = new BMapGL.Label(text, {
+      position: new BMapGL.Point(p.longitude, p.latitude),
+      offset: new BMapGL.Size(-20, -30),
+    });
+    label.setStyle({
+      // 设置label的样式
+      color: color || "#000",
+      fontSize: "10px",
+      border: "1px solid #1E90FF",
+    });
+    this.map.addOverlay(label);
+    this.map.addOverlay(marker);
+  }
 }
